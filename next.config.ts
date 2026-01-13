d025dd24619b0c73803bce861f54f7cdea804613
jvieirasilva <jvieirasilva@gmail.com>
2026-01-13 19:45:45 +0000

    order

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://zl0pbu4u85.execute-api.us-east-1.amazonaws.com/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
